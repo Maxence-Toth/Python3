06443efc2bfd83e26e9deb660ffdca0fe8305471
Moseus <toth.maxence@gmail.com>
2022-02-28 16:04:50 +0100

    Création du répo
    Ajout des premiers chapitre de cours .
    Ajout des exo correspondants.

---
+++ variables.py
@@ -0,0 +1,23 @@
+nombre = 123
+print(nombre)
+
+nombre = "123"
+print(nombre)
+
+text ="lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print (has_accepted_ula)
+
+html_code = '<h1>Titre de mon blog</h1>'
+print(html_code)
+
+nickname = "Jhon \"Dead\" Doe"
+print(nickname)
